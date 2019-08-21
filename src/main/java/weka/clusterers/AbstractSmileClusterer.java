c6899443e549bd7c28517a14ffc6e5bc6e607521
Peter Reutemann <fracpete@gmail.com>
2019-08-21 12:25:33 +1200

    basic clustering support

---
+++ src/main/java/weka/clusterers/AbstractSmileClusterer.java
@@ -0,0 +1,113 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * AbstractSmileClusterer.java
+ * Copyright (C) 2019 University of Waikato, Hamilton, NZ
+ */
+
+package weka.clusterers;
+
+import smile.clustering.PartitionClustering;
+import smile.data.AttributeDataset;
+import weka.core.DatasetUtils;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Utils;
+
+/**
+ * Ancestor for SMILE clustering algorithms.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public abstract class AbstractSmileClusterer
+  extends AbstractClusterer {
+
+  private static final long serialVersionUID = 8061087017316008521L;
+
+  /** the dataset structure. */
+  protected AttributeDataset m_Header;
+
+  /** the model. */
+  protected smile.clustering.Clustering<double[]> m_Model;
+
+  /**
+   * Builds the clusterer.
+   *
+   * @param data	the data to use for training
+   * @return 		the generated model
+   * @throws Exception	if training fails or data does not match capabilities
+   */
+  protected abstract smile.clustering.Clustering<double[]> buildClusterer(AttributeDataset data) throws Exception;
+
+  /**
+   * Builds the clusterer.
+   *
+   * @param data	the data to use for training
+   * @throws Exception	if training fails or data does not match capabilities
+   */
+  @Override
+  public void buildClusterer(Instances data) throws Exception {
+    AttributeDataset	dataset;
+
+    getCapabilities().testWithFail(data);
+    dataset = DatasetUtils.convertInstances(data);
+    m_Header = dataset.head(0);
+    m_Model  = buildClusterer(dataset);
+  }
+
+  /**
+   * Classifies the specified instance.
+   *
+   * @param instance	the instance to classify
+   * @return		the classification
+   * @throws Exception	if classification fails
+   */
+  @Override
+  public int clusterInstance(Instance instance) throws Exception {
+    double[]	values;
+
+    values = DatasetUtils.convertInstance(instance, m_Header);
+    return m_Model.predict(values);
+  }
+
+  /**
+   * Returns the number of clusters.
+   *
+   * @return the number of clusters generated for a training dataset.
+   * @throws Exception if number of clusters could not be returned
+   *              successfully
+   */
+  @Override
+  public int numberOfClusters() throws Exception {
+    if (m_Model instanceof PartitionClustering)
+      return ((PartitionClustering<double[]>) m_Model).getNumClusters();
+    else
+      throw new IllegalStateException("Retrieval of number of clusters is not supported!");
+  }
+
+  /**
+   * Outputs some information about the model.
+   *
+   * @return		the model
+   */
+  @Override
+  public String toString() {
+    if (m_Model == null)
+      return Utils.toCommandLine(this) + "\n" + "No model built yet!\n";
+    else
+      return Utils.toCommandLine(this) + "\n" + m_Model.getClass().getName() + "\n";
+  }
+}
---
+++ src/main/java/weka/clusterers/SmileKMeans.java
@@ -0,0 +1,79 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * SmileKMeans.java
+ * Copyright (C) 2019 University of Waikato, Hamilton, NZ
+ */
+
+package weka.clusterers;
+
+import smile.clustering.Clustering;
+import smile.clustering.KMeans;
+import smile.data.AttributeDataset;
+import weka.core.Capabilities;
+import weka.core.Capabilities.Capability;
+
+/**
+ * Wraps the SMILE KMeans algorithm.
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ */
+public class SmileKMeans
+  extends AbstractSmileClusterer {
+
+  private static final long serialVersionUID = -9151643463590262607L;
+
+  /**
+   * Returns default capabilities of the clusterer.
+   *
+   * @return the capabilities of this clusterer
+   */
+  @Override
+  public Capabilities getCapabilities() {
+    Capabilities result = super.getCapabilities();
+    result.disableAll();
+    result.enable(Capability.NO_CLASS);
+
+    // attributes
+    result.enable(Capability.NOMINAL_ATTRIBUTES);
+    result.enable(Capability.NUMERIC_ATTRIBUTES);
+    result.enable(Capability.MISSING_VALUES);
+
+    return result;
+  }
+
+  /**
+   * Builds the clusterer.
+   *
+   * @param data	the data to use for training
+   * @return 		the generated model
+   * @throws Exception	if training fails or data does not match capabilities
+   */
+  @Override
+  protected Clustering<double[]> buildClusterer(AttributeDataset data) throws Exception {
+    return new KMeans(data.x(), 3);
+  }
+
+  /**
+   * Executes the clusterer from the commandline.
+   *
+   * @param args	the parameters to use
+   * @throws Exception	if execution fails
+   */
+  public static void main(String[] args) throws Exception {
+    runClusterer(new SmileKMeans(), args);
+  }
+}
